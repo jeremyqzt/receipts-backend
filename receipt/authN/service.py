6d046fea0eb9b45773bb8e068edfa9e14a82f708
Jeremy Qian <jq357@hotmail.com>
2023-06-21 22:09:34 -0400

    reading up on resources

---
+++ receipt/authN/service.py
@@ -0,0 +1,8 @@
+
+class PasswordService:
+    def __init__(self):
+        ...
+
+    def sendRecoveryEmail(self):
+        ...
+        # Check this out https://support.google.com/domains/answer/9437157?hl=en#:~:text=In%20the%20%22Send%20mail%20as,marked%2C%20and%20click%20Next%20step.
