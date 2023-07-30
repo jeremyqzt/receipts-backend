4dfa5a4249c861e6e7e89c86579219f2e20877a1
Jeremy Qian <jq357@hotmail.com>
2023-07-29 21:07:16 -0400

    enable again

--- receipt/authN/views.py
+++ receipt/authN/views.py
@@ -159,13 +159,13 @@ class UserForgotPasswordResetFormView(APIView):
 
         try:
             inst = PasswordReset.objects.get(username=username)
-            #sendRecoveryEmail(username, str(inst.uuid))
+            sendRecoveryEmail(username, str(inst.uuid))
         except ObjectDoesNotExist:
             ...
 
         try:
             PasswordReset.objects.create(username=username, uuid=reset_id)
-            #sendRecoveryEmail(username, reset_id)
+            sendRecoveryEmail(username, reset_id)
         except:
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR, data={})
 
