f15bc029365bb8d8d704814ec2ae7bd25aaab580
Jeremy Qian <jq357@hotmail.com>
2023-08-01 21:15:44 -0400

    testing indices

--- receipt/authN/models.py
+++ receipt/authN/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 class PasswordReset(models.Model):
+    class Meta:
+        indexes = [
+            models.Index(fields=['username', 'uuid'], name='reset_idx'),
+        ]
     username = models.TextField()
     uuid = models.UUIDField()
     timeStamp = models.DateTimeField(auto_now=True)
