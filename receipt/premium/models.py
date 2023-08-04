5e1cf50e584d2b99867c076b3f52e75487345dcd
Jeremy Qian <jq357@hotmail.com>
2023-08-03 20:42:36 -0400

    add models

--- receipt/premium/models.py
+++ receipt/premium/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ProcessedState(models.IntegerChoices):
+    UNLIMITED_RECEIPTS = 1
+    UNLIMITED_BUCKETS_RECEIPTS = 2
+
+class Premium(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    level = models.IntegerField(
+        choices=ProcessedState.choices, default=ProcessedState.UPLOADED
+    )
