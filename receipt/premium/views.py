bcd369e4cc0388393661237c73eef170216f574b
Jeremy Qian <jq357@hotmail.com>
2023-08-04 21:21:23 -0400

    basic view setup

--- receipt/premium/views.py
+++ receipt/premium/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from uploader.serializers import UploadedItemSerializer, UploadedItemCreateSerializer, ReceiptDeleteSerializer, ReceiptReParseSerializer
+from rest_framework.permissions import IsAuthenticated
+from uploader.constants import RECEIPT_COUNT_DEFAULT, RECEIPT_COUNT_LIMIT
+from uploader.receiptService import ReceiptService
+from buckets.service import BucketService
 
 # Create your views here.
+class PremiumSubscribeView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        return Response(request.data)
