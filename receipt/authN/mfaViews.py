b1eb173d07559bd482762f4a6dd526338799ab67
Jeremy Qian <jq357@hotmail.com>
2023-09-24 21:10:02 -0400

    return view instead of bool

--- receipt/authN/mfaViews.py
+++ receipt/authN/mfaViews.py
@@ -4,11 +4,14 @@ from rest_framework import status
 from django_otp import devices_for_user
 from django_otp.plugins.otp_totp.models import TOTPDevice
 
+
 def get_user_totp_device(self, user, confirmed=None):
     devices = devices_for_user(user, confirmed=confirmed)
     for device in devices:
         if isinstance(device, TOTPDevice):
             return device
+
+
 class TOTPCreateView(views.APIView):
     """
     Use this endpoint to set up a new TOTP device
@@ -19,9 +22,10 @@ class TOTPCreateView(views.APIView):
         user = request.user
         device = get_user_totp_device(self, user)
 
-        if device: return True
+        if device:
+            return Response(False, status=status.HTTP_200_OK)
 
-        return False
+        return Response(True, status=status.HTTP_200_OK)
 
     def post(self, request, format=None):
         user = request.user
@@ -31,11 +35,13 @@ class TOTPCreateView(views.APIView):
         url = device.config_url
         return Response(url, status=status.HTTP_201_CREATED)
 
+
 class TOTPVerifyView(views.APIView):
     """
     Use this endpoint to verify/enable a TOTP device
     """
     permission_classes = [permissions.IsAuthenticated]
+
     def post(self, request, token, format=None):
         user = request.user
         device = get_user_totp_device(self, user)
